defd94997a29c7a78ff9e398760a716da2a093d6
laijihua <1164258202@qq.com>
2023-06-08 00:36:40 +0800

    脚本

---
+++ README.md
@@ -0,0 +1,3 @@
+# 打包
+
+文档：[https://juejin.cn/post/7241943960083169339](https://juejin.cn/post/7241943960083169339)
--- package.sh
+++ package.sh
@@ -1,4 +1,4 @@
-# docker build -t package-docker-image .
+docker build -t package-docker-image .
 docker create --name temporary-container package-docker-image
 rm -rf ./PackageApp
 docker cp temporary-container:/staging ./PackageApp
