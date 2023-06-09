cd29ae3222b57b828a8b7bf4773316d789307046
laijihua <1164258202@qq.com>
2023-06-09 12:43:50 +0800

    build.sh open

--- package.sh
+++ package.sh
@@ -1,4 +1,4 @@
-# docker build -t package-docker-image .
+docker build -t package-docker-image .
 docker create --name temporary-container package-docker-image
 docker cp temporary-container:/staging.tar.gz ./PackageApp.zip
 docker rm temporary-container
